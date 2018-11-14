f2a1a46aa48d9426371fc0db8e203bd760f7cafe
Karraymahdi <38590938+Karraymahdi@users.noreply.github.com>
2018-11-14 13:07:45 +0100

    Update TEST.java

--- Training_Project_Lab/src/Package/TEST.java
+++ Training_Project_Lab/src/Package/TEST.java
@@ -7,8 +7,8 @@ import java.io.*;
 public class TEST {
 	 private static String url = "";
   
-	public static void POST(String route, NameValuePair[] data,int expectedStatusCode)
-	{
+	public static boolean POST(String route, NameValuePair[] data,int expectedStatusCode)
+	{	boolean result = false ;
 		// Create an instance of HttpClient.
 	    HttpClient client = new HttpClient();
 	    // Create a method instance.
@@ -16,27 +16,28 @@ public class TEST {
 	    // Provide custom retry handler is necessary
 	    client.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, 
 	    		  new DefaultHttpMethodRetryHandler());
-	    try {
-	    	 post.addParameters(data);
-	        // Execute the method.
-	        int statusCode = client.executeMethod(post);
-	        Assert.assertEquals(expectedStatusCode, statusCode);
-	        System.out.println("succeeded");
-
-	      } catch (HttpException e) {
-	        System.err.println(" protocol violation: " + e.getMessage());
-	        e.printStackTrace();
-	      } catch (IOException e) {
-	        System.err.println(" transport error: " + e.getMessage());
-	        e.printStackTrace();
-	      } finally {
-	        // Release the connection.
-	        post.releaseConnection();
-	      }  
+	    post.addParameters(data);
+        // Execute the method.
+        try {
+			int statusCode = client.executeMethod(post);
+			if (statusCode==expectedStatusCode)
+				result = true; 
+			
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        finally {
+	       post.releaseConnection();
+        }
+		return result;
 	}
 	@SuppressWarnings("deprecation")
-	public static void PUT(String route, String  data,int expectedStatusCode)
-	{
+	public static boolean PUT(String route, String  data,int expectedStatusCode)
+	{	boolean result = false ;
 		// Create an instance of HttpClient.
 	    HttpClient client = new HttpClient();
 	    // Provide custom retry handler is necessary
@@ -55,23 +56,24 @@ public class TEST {
 	    try {
 	        // Execute the method.
 	        int statusCode = client.executeMethod(put);
-	        Assert.assertEquals(expectedStatusCode, statusCode);
-	        	System.out.println("hello");
+	        if (statusCode==expectedStatusCode)
+				result = true; 
 
 	      } catch (HttpException e) {
-	        System.err.println("Fatal protocol violation: " + e.getMessage());
-	        e.printStackTrace();
-	      } catch (IOException e) {
-	        System.err.println("Fatal transport error: " + e.getMessage());
-	        e.printStackTrace();
-	      } finally {
-	        // Release the connection.
-	        put.releaseConnection();
-	      }  
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        finally {
+		       put.releaseConnection();
+	        }
+	    return result;
 	}
-	public static void Get(String route,int expectedStatusCode)
+	public static boolean  Get(String route,int expectedStatusCode)
 	
-	{
+	{	boolean result = false ;
 		// Create an instance of HttpClient.
 	    HttpClient client = new HttpClient();
 	    // Create a method instance.
@@ -82,21 +84,25 @@ public class TEST {
 	    try {
 	        // Execute the method.
 	        int statusCode = client.executeMethod(method);
-	        Assert.assertEquals(expectedStatusCode, statusCode);
+	        if (statusCode==expectedStatusCode)
+				result = true; 
+
 	      } catch (HttpException e) {
-	        System.err.println("Fatal protocol violation: " + e.getMessage());
-	        e.printStackTrace();
-	      } catch (IOException e) {
-	        System.err.println("Fatal transport error: " + e.getMessage());
-	        e.printStackTrace();
-	      } finally {
-	        // Release the connection.
-	        method.releaseConnection();
-	      }  
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        finally {
+		       method.releaseConnection();
+	        }
+	    return result;
 	}
 	
 	
-	public static void DELETE(String route,int expectedStatusCode) throws Exception {
+	public static boolean DELETE(String route,int expectedStatusCode) throws Exception {
+			boolean result = false ;
 		  HttpClient client=new HttpClient();
 		  DeleteMethod method=new DeleteMethod(url+route);
 		  //client.executeMethod(method);
@@ -104,22 +110,27 @@ public class TEST {
 		  try {
 		        // Execute the method.
 		        int statusCode = client.executeMethod(method);
-		        Assert.assertEquals(expectedStatusCode, statusCode);
+		        if (statusCode==expectedStatusCode)
+					result = true; 
+
 		      } catch (HttpException e) {
-		        System.err.println("Fatal protocol violation: " + e.getMessage());
-		        e.printStackTrace();
-		      } catch (IOException e) {
-		        System.err.println("Fatal transport error: " + e.getMessage());
-		        e.printStackTrace();
-		      } finally {
-		        // Release the connection.
-		        method.releaseConnection();
-		      }  
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        finally {
+			       method.releaseConnection();
+		        }
+		    return result;
 		  //if (status == 404)   throw new IllegalArgumentException("Dataset does not exist.");
 		 // if (status == 503)   throw new IllegalStateException("Service error: " + status);
 		}
 	
   public static void main(String[] args) {
+	  //exemple
+	 System.out.println(Get("https://20181102t154318-dot-training-project-lab.appspot.com/?fbclid=IwAR2BNcantGQsRYCmG-0DjnLjY7J8m_7hXJQ02K-yem_R7_QVNDXekPUYmpY",400)); 
 
   }
 }
